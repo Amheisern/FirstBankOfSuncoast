69bb16f7bad2828662a9df643fd80d8560e5627e
lodashreload <icarusjapan@gmail.com>
2021-12-31 20:52:13 -0500

    moved trans and trans database to own file

--- Transaction.cs
+++ Transaction.cs
@@ -4,10 +4,15 @@ namespace FirstBankOfSuncoast
     {
         public class Transaction
         {
+            // checking or savings
             public string Account { get; set; }
+            //How much
             public string Amount { get; set; }
+            // Deposit or Withdraw
             public string Type { get; set; }
             // public DateTime Date { get; set; } = DateTime.Now;
+
+
         }
     }
 }
---
+++ TransactionDatabase.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace FirstBankOfSuncoast
+{
+    partial class Program
+    {
+        class TransactionDatabase
+        {
+            public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+
+        }
+    }
+}
